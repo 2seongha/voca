ed915b6adb80dea75d6098826f8c2d27631bdc82
2seongha <tjdgkwlwhs@naver.com>
2023-04-15 02:08:04 +0900

    second commit

--- app/app.js
+++ app/app.js
@@ -29,7 +29,7 @@ app.use(express.static(`${__dirname}/src/views/home/voca/build`));
 app.use(express.json());
 //url을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
 app.use(express.urlencoded({extended:true}));
-app.use("/", home); // use -> 미들웨어 등록해주는 메소드 입니다.
+app.use("/", home); // use -> 미들웨어 등록해주는 메소드 입니다. dd
 
 
 module.exports = app;
